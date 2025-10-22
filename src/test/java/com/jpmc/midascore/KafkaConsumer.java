a5fd83a15124e8197ae1e811334c07d95ff014c7
raGnok0 <baruadeb058@gmail.com>
2025-10-22 22:44:04 +0530

    Update Kafka configuration in application.yml and add KafkaConsumer class

---
+++ src/test/java/com/jpmc/midascore/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.jpmc.midascore;
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core-group")
+    public void consume(Transaction transaction) {
+        System.out.println("Received transaction: " + transaction);
+    }
+}
